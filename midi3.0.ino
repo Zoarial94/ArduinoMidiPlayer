db1d064dffd0fa131b564b60e936ac44cc854b94
Zoarial94 <hunterw9469@gmail.com>
2018-11-08 14:44:58 -0500

    Update midi3.0.ino

--- midi3.0.ino
+++ midi3.0.ino
@@ -1,18 +1,18 @@
 #include "MidiFile.hpp"
 
-Midi::MidiFile* doom;
+Midi::MidiFile* doom; //Pointer to class
 
 void setup() {
-  Serial.begin(1000000);
-  Serial1.begin(31250);
+  Serial.begin(1000000);           //COMM with computer
+  Serial1.begin(31250);            //COMM with midi device
   
-  doom = new Midi::MidiFile();
+  doom = new Midi::MidiFile();     //Initialize class
 
-  doom->open("flam.mid");
-  doom->start();
-  while(!doom->isFinished()) {
-    //Serial.println(freeMemory());
-    doom->loop();
+  doom->open("flam.mid");          //Open midi file from SD Card
+  doom->start();                   //Initialize part of class
+  
+  while(!doom->isFinished()) {     //While there are events to play...
+    doom->loop();                  //do stuff...
   }
 
   Serial.println(F("MIDI IS DONE"));
